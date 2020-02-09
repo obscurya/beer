b989ca4bc07883ea5db8b7ff9425976746c4ed9b
obscurya <prostunting@mail.ru>
2020-02-09 15:47:05 +0300

    Updates

---
+++ precache-manifest.c2cbbd5dad8371918ef9fc8f3935fe50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de9fa578048d70ca09c6f21439e1be7",
+    "url": "/beer/index.html"
+  },
+  {
+    "revision": "f0398c81b2fb65fffcb7",
+    "url": "/beer/static/css/main.8155bc82.chunk.css"
+  },
+  {
+    "revision": "40a2399468e33880bb44",
+    "url": "/beer/static/js/2.376a0d44.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/beer/static/js/2.376a0d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0398c81b2fb65fffcb7",
+    "url": "/beer/static/js/main.5aba357c.chunk.js"
+  },
+  {
+    "revision": "523b05e2a906a4d923cc",
+    "url": "/beer/static/js/runtime-main.dd28b84c.js"
+  }
+]);
